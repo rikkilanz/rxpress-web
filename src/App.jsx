c0c95fefb356b6a0d8c31acfd3dbadc0b96978c4
Monica Zarate <monicazaratecortes@gmail.com>
2024-01-20 14:10:45 -0800

    Initial project structure, router set-up.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Dashboard from './components/Dashboard'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className='text-red-500 text-xl font-bold'>HOLA</h1>
-    </>
+    <Dashboard/>
   )
 }
 
---
+++ src/assets/index.js
@@ -0,0 +1,3 @@
+import logo from "./rxpress-logo.png";
+
+export { logo };
