f3411f4d581dbfddae97c87ff681c88469b3609d
davekoo1106 <114951120+davekoo1106@users.noreply.github.com>
2024-01-20 20:07:33 -0800

    Created Previous orders and Upcoming order list

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 export default function Dashboard() {
     return (
         <main className="bg-indigo-900 text-white">
-            <div className="">Main container here</div>
+            <div>Main container here</div>
         </main>
     )
 }
