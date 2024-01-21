99ca23bb1cd89704dc73d87cfef21bfebf796e5d
davekoo1106 <114951120+davekoo1106@users.noreply.github.com>
2024-01-20 21:37:18 -0800

    Created Patient Doctor page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Landing from "./components/Landing";
 import Login from "./components/Login";
 import Dashboard from './components/Dashboard';
 import PatientDetails from './components/PatientDetails';
+import PatientDoctor from './components/PatientDoctor';
 
 function App() {
   const [profile, setProfile] = useState(null);
@@ -28,6 +29,7 @@ function App() {
             <Route path="/patients" element={<Patients />} />
             <Route path="/patient/:id" element={<PatientDetails/>}/>
             <Route path="/patient-dash" element={<PatientDash/>} />
+            <Route path="/doctors" element={<PatientDoctor/>} />
           </Routes>
         </div>
       )}
--- src/components/PatientDash.jsx
+++ src/components/PatientDash.jsx
@@ -10,62 +10,8 @@ import newOrder from "../data/newOrderData.json";
 export default function PatientDash() {
     return(
         <main className="col-span-3">
-
-            {/* Past Orders Section */}
-            <div className="text-primary-300 text-2xl mt-5">
-                <h1>Your Past Orders</h1>
-            </div>
-
-            {/* Filter Section ( May not get to it but if possible would be a nice feature) */}
-            <div className="flex justify-center items-center gap-40 mt-10">
                 <div>
-                    <h2>From:</h2>
-                </div>
-                <div>
-                    <h2>To:</h2>
-                </div>
-                <div>
-                    <button className="bg-blue-400 text-white p-2 rounded-lg">Submit</button>
-                </div>
-            </div>
-
-
-            <div>
-                <div className="text-black flex flex-col pb-10 p-10 mt-5 bg-slate-100">
-                    {data.map((item) => (
-                            <div className="flex gap-20 items-center" key={item.id}>
-                                <div className="m-2">
-                                    <h2 className="text-primary underline pb-2">Order Id</h2>
-                                    <h3>{item.order_id}</h3>
-                                </div>
-                                <div className="m-2">
-                                    <h2 className="text-primary underline pb-2">Order Status</h2>
-                                    <h3>{item.order_status}</h3>
-                                </div>
-                                <div className="m-2">
-                                    <h2 className="text-primary underline pb-2">Date</h2>
-                                    <h3>{item.date}</h3>
-                                </div>
-                                <div className="m-2">
-                                    <h2 className="text-primary underline pb-2">Doctor</h2>
-                                    <h3>{item.doctor}</h3>
-                                </div>
-                                <div className="m-2">
-                                    <h2 className="text-primary underline pb-2">Company</h2>
-                                    <h3>{item.company}</h3>
-                                </div>
-                                <div className="m-2">
-                                    <h2 className="text-primary underline pb-2">Total</h2>
-                                    <h3>{item.total}</h3>
-                                </div>
-                            </div>
-                        ))}
-                </div>
-
-                {/* Upcoming Order Section */}
-                    
-                <div>
-                    <div className="text-primary-300 text-2xl mt-5">
+                    <div className="text-primary-300 text-2xl mt-5 ml-4">
                         <h2>Upcoming Orders</h2>
                     </div>  
                         
@@ -108,6 +54,60 @@ export default function PatientDash() {
                             ))}
                     </div>
                 </div>
+            
+            {/* Past Orders Section */}
+            <div className="text-primary-300 text-2xl mt-5 ml-4">
+                <h1>Your Past Orders</h1>
+            </div>
+
+            {/* Filter Section ( May not get to it but if possible would be a nice feature) */}
+            <div className="flex justify-center items-center gap-40 mt-10">
+                <div>
+                    <h2>From:</h2>
+                </div>
+                <div>
+                    <h2>To:</h2>
+                </div>
+                <div>
+                    <button className="bg-blue-400 text-white p-2 rounded-lg">Submit</button>
+                </div>
+            </div>
+
+
+            <div>
+                <div className="text-black flex flex-col pb-10 p-10 mt-5 bg-slate-100">
+                    {data.map((item) => (
+                            <div className="flex gap-20 items-center" key={item.id}>
+                                <div className="m-2">
+                                    <h2 className="text-primary underline pb-2">Order Id</h2>
+                                    <h3>{item.order_id}</h3>
+                                </div>
+                                <div className="m-2">
+                                    <h2 className="text-primary underline pb-2">Order Status</h2>
+                                    <h3 className="bg-green-300 rounded-full text-center">{item.order_status}</h3>
+                                </div>
+                                <div className="m-2">
+                                    <h2 className="text-primary underline pb-2">Date</h2>
+                                    <h3>{item.date}</h3>
+                                </div>
+                                <div className="m-2">
+                                    <h2 className="text-primary underline pb-2">Doctor</h2>
+                                    <h3>{item.doctor}</h3>
+                                </div>
+                                <div className="m-2">
+                                    <h2 className="text-primary underline pb-2">Company</h2>
+                                    <h3>{item.company}</h3>
+                                </div>
+                                <div className="m-2">
+                                    <h2 className="text-primary underline pb-2">Total</h2>
+                                    <h3>{item.total}</h3>
+                                </div>
+                            </div>
+                        ))}
+                </div>
+
+                {/* Upcoming Order Section */}
+                    
             </div>
         </main>
     );
