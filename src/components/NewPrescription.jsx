8587c67ec40812d5c99c5113dded6f1e1dddc27e
Rikki Soriano <rikkilanz@gmail.com>
2024-01-21 00:43:53 -0800

    Styling tweaks

--- src/components/NewPrescription.jsx
+++ src/components/NewPrescription.jsx
@@ -1,10 +1,14 @@
 import { patientData } from "../data/patientData";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
+import { CheckCircleIcon, ArrowLeftIcon } from "@heroicons/react/24/solid";
+import { NavLink } from "react-router-dom";
 
-export default function NewPrescription() {
+export default function NewPrescription({ prescriptionFlow }) {
   const [selectedPatient, setSelectedPatient] = useState(null);
   const [prescriptionData, setPrescriptionData] = useState(null);
+  const [reviewScreen, setReviewScreen] = useState(false);
+  const [confirmScreen, setConfirmScreen] = useState(false);
 
   const handleSelectChange = (event) => {
     const selectedPatientId = event.target.value;
@@ -15,81 +19,168 @@ export default function NewPrescription() {
   };
 
   return (
-    <div className="col-span-3 px-4 overflow-y-scroll">
-      <div className="sticky top-0 w-full bg-white">
-        <h1 className="text-[48px] font-bold py-10">New Prescription</h1>
-      </div>
-      <div className="px-4">
-        <XMarkIcon className="h-8 w-8" />
-        <h2 className="text-[24px]">For which patient?</h2>
-        <div className="relative inline-block text-left mt-4">
-          <select
-            className="block appearance-none w-full bg-white border border-gray-300 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:border-blue-500"
-            onChange={handleSelectChange}
-          >
-            {patientData.map((patient) => (
-              <option key={patient.id} value={patient.id}>
-                {patient.name}
-              </option>
-            ))}
-          </select>
-          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-            <svg
-              className="fill-current h-4 w-4"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
+    <div className="bg-sky-50/75 col-span-3 py-16 px-8 overflow-y-scroll">
+      {!reviewScreen && !confirmScreen ? (
+        <div>
+          <div className="flex items-center">
+            <button
+              onClick={() => {
+                prescriptionFlow(false);
+              }}
             >
-              <path d="M0 0h20v20H0z" fill="none" />
-              <path d="M10 12L4 6h12l-6 6z" fill="#4A5568" />
-            </svg>
+              <ArrowLeftIcon className="h-8 w-8 text-primary" />
+            </button>
+            <h1 className="text-sky-600 ml-8 text-[48px] font-bold">
+              New Prescription
+            </h1>
           </div>
-        </div>
-      </div>
-      {selectedPatient && (
-        <div className="mt-4 col-span-3 px-4">
-          <div>
-            <h3 className="text-lg font-semibold">Selected Patient:</h3>
-            <p>Name: {selectedPatient.name}</p>
-            <p>Address: {selectedPatient.address}</p>
-            <p>
-              Preferred Pharmacy Location:{" "}
-              {selectedPatient.preferredPharmacyLocation}
-            </p>
-            <p>Phone Number: {selectedPatient.phoneNumber}</p>
-            <p>Email: {selectedPatient.email}</p>
-            <p>Date of Birth: {selectedPatient.dateOfBirth}</p>
-          </div>
-          <div className="">
-            <h3 className="text-lg font-semibold">What is the prescription?</h3>
-            <textarea
-              name=""
-              id=""
-              cols="30"
-              rows="10"
-              className="border rounded-md border-gray-500"
-              onChange={(e) => setPrescriptionData(e.target.value)}
-            ></textarea>
-          </div>
-          {prescriptionData && (
-            <div className="flex justify-between">
-              <button
-                onClick={() => {
-                  setPrescriptionFlow(true);
-                }}
-                className="border border-gray-200 py-2 px-4 rounded-md"
+          <div className="px-4">
+            <h2 className="text-[24px] mt-8">
+              Which patient are you creating a prescription for?
+            </h2>
+            <div className="relative inline-block text-left mt-4">
+              <select
+                className="block appearance-none w-full bg-white border border-gray-300 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:border-blue-500"
+                onChange={handleSelectChange}
               >
-                Cancel
-              </button>
-              <button
-                onClick={() => {
-                  setPrescriptionFlow(true);
-                }}
-                className="bg-primary text-white py-2 px-4 rounded-md"
-              >
-                Review
-              </button>
+                <option value="" disabled selected>
+                  Select a patient
+                </option>
+                {patientData.map((patient) => (
+                  <option key={patient.id} value={patient.id}>
+                    {patient.name}
+                  </option>
+                ))}
+              </select>
+              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <svg
+                  className="fill-current h-4 w-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M0 0h20v20H0z" fill="none" />
+                  <path d="M10 12L4 6h12l-6 6z" fill="#4A5568" />
+                </svg>
+              </div>
+            </div>
+          </div>
+          {selectedPatient && (
+            <div className="mt-4 col-span-3 px-4">
+              <div className="bg-white p-8 rounded shadow-md border border-sky-200">
+                <h3 className="text-lg font-semibold">Selected Patient:</h3>
+                <p>Name: {selectedPatient.name}</p>
+                <p>Address: {selectedPatient.address}</p>
+                <p>
+                  Preferred Pharmacy Location:{" "}
+                  {selectedPatient.preferredPharmacyLocation}
+                </p>
+                <p>Phone Number: {selectedPatient.phoneNumber}</p>
+                <p>Email: {selectedPatient.email}</p>
+                <p>Date of Birth: {selectedPatient.dateOfBirth}</p>
+              </div>
+              <div className="">
+                <h3 className="text-lg font-semibold mt-8 mb-2">
+                  What is the prescription?
+                </h3>
+                <textarea
+                  name=""
+                  placeholder="Enter prescription details here..."
+                  id=""
+                  rows="5"
+                  className="border rounded-md border-primary-300 focus:border-1 min-w-full p-4 focus:outline-none focus:ring focus:ring-primary"
+                  onChange={(e) => setPrescriptionData(e.target.value)}
+                ></textarea>
+              </div>
+              {prescriptionData && (
+                <div className="flex justify-between mt-4 pb-8">
+                  <button
+                    onClick={() => {
+                      prescriptionFlow(false);
+                    }}
+                    className="border border-gray-200 py-2 px-4 rounded-md bg-white"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    onClick={() => {
+                      setReviewScreen(true);
+                    }}
+                    className="bg-primary text-white py-2 px-4 rounded-md"
+                  >
+                    Review
+                  </button>
+                </div>
+              )}
             </div>
           )}
+        </div>
+      ) : !confirmScreen ? (
+        <div className="">
+          <div className="">
+            <h1 className="text-sky-600 mb-8 text-[48px] font-bold">
+              Prescription Review
+            </h1>
+          </div>
+          <div className="mt-4 col-span-3 px-4 bg-white p-8 rounded shadow-md border border-sky-200">
+            <div>
+              <h3 className="text-lg font-semibold">Prescription for:</h3>
+              <p>Name: {selectedPatient.name}</p>
+              <p>Address: {selectedPatient.address}</p>
+
+              <p>Phone Number: {selectedPatient.phoneNumber}</p>
+              <p>Email: {selectedPatient.email}</p>
+              <p>Date of Birth: {selectedPatient.dateOfBirth}</p>
+            </div>
+          </div>
+          <div className="mt-4 col-span-3 px-4 bg-white p-8 rounded shadow-md border border-sky-200">
+            <div>
+              <h3 className="text-lg font-semibold">
+                Sending prescription to:
+              </h3>
+              <p>
+                Preferred Pharmacy Location:{" "}
+                {selectedPatient.preferredPharmacyLocation}
+              </p>
+            </div>
+          </div>
+          <div className="mt-4 col-span-3 px-4 bg-white p-8 rounded shadow-md border border-sky-200">
+            <div>
+              <h3 className="text-lg font-semibold">Prescription details:</h3>
+              <p>{prescriptionData}</p>
+            </div>
+          </div>
+          <div className="flex justify-between mt-4">
+            <button
+              onClick={() => {
+                prescriptionFlow(false);
+              }}
+              className="border border-gray-200 py-2 px-4 rounded-md bg-white"
+            >
+              Go Back
+            </button>
+            <button
+              onClick={() => {
+                setConfirmScreen(true);
+              }}
+              className="bg-primary text-white py-2 px-4 rounded-md"
+            >
+              Confirm
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className="col-span-3 px-4 py-8 text-center flex flex-col items-center justify-center h-full">
+          <CheckCircleIcon className="h-10 w-10 text-primary" />
+          <h3 className="font-playfair text-[32px]">
+            Prescription sent to pharmacy!
+          </h3>
+          <p>We will notify your patient about their prescription.</p>
+          <NavLink
+            to="/"
+            className="bg-primary text-white py-2 px-4 rounded-md mt-8"
+          >
+            Back to Dashboard
+          </NavLink>
         </div>
       )}
     </div>
--- src/components/Prescription.jsx
+++ src/components/Prescription.jsx
@@ -5,6 +5,7 @@ import {
 } from "../data/doctorsPrescriptionData";
 import NewPrescription from "./NewPrescription";
 import { useState } from "react";
+import { TruckIcon, ClockIcon } from "@heroicons/react/24/outline";
 
 export default function Prescription() {
   const [prescriptionFlow, setPrescriptionFlow] = useState(false);
@@ -23,50 +24,85 @@ export default function Prescription() {
   );
 
   return prescriptionFlow ? (
-    <NewPrescription />
+    <NewPrescription prescriptionFlow={setPrescriptionFlow} />
   ) : (
-    <div className="col-span-3 px-4 overflow-y-scroll">
-      <div className="sticky top-0 w-full bg-white">
-        <h1 className="text-[48px] font-bold py-10">Prescriptions</h1>
+    <div className="bg-sky-50/75 col-span-3 py-16 px-8 overflow-y-scroll">
+      <div className="">
+        <h1 className="text-sky-600 mb-8 text-[48px] font-bold">
+          Prescriptions
+        </h1>
       </div>
 
-      <div className="mt-8">
-        <div className="flex justify-between align-center">
-          <h3 className="text-[24px]">Prescription Details</h3>
-          <button
-            onClick={() => {
-              setPrescriptionFlow(true);
-            }}
-            className="bg-primary text-white py-2 px-4 rounded-md"
-          >
-            New Prescription +
-          </button>
+      <button
+        onClick={() => {
+          setPrescriptionFlow(true);
+        }}
+        className="bg-primary text-white py-2 px-4 rounded-md"
+      >
+        New Prescription +
+      </button>
+      <div className="mt-8 bg-white p-8 rounded shadow-md border border-sky-200">
+        <div className="flex align-center">
+          <ClockIcon className="h-8 w-8 text-sky-900" />
+          <h3 className="ml-4 text-[24px] mb-4 text-sky-900">
+            Pending prescriptions
+          </h3>
         </div>
 
         <div className="mt-2 max-h-[300px] overflow-y-scroll">
-          <div className="flex bg-gray-200 rounded-md shadow-md p-4 mb-4 sticky top-0">
+          <div className="flex bg-sky-50 text-xl text-sky-900 rounded-md shadow-md p-4 mb-4 sticky top-0">
             <div className="w-1/5">
-              <h4 className="font-semibold">Patient Name</h4>
+              <h4>Patient Name</h4>
             </div>
             <div className="w-1/5">
-              <h4 className="font-semibold">Checkin Id</h4>
+              <h4>Check-In ID</h4>
             </div>
             <div className="w-1/5">
-              <h4 className="font-semibold">Prescription Name</h4>
+              <h4>Prescription Name</h4>
             </div>
             <div className="w-2/5">
-              <h4 className="font-semibold">Pharmacy Location</h4>
+              <h4>Pharmacy Location</h4>
             </div>
             <div className="w-1/5">
-              <h4 className="font-semibold">Date Sent</h4>
+              <h4>Date Sent</h4>
             </div>
           </div>
 
           {pendingPrescriptions.map(renderPrescriptionRow)}
         </div>
       </div>
+      <div className="mt-8 bg-white p-8 rounded shadow-md border border-sky-200">
+        <div className="flex align-center">
+          <TruckIcon className="h-8 w-8 text-sky-900" />
+          <h3 className="ml-4 text-[24px] mb-4 text-sky-900">
+            Prescription dropped off
+          </h3>
+        </div>
 
-      <div className="mt-8 max-h-[300px] overflow-y-scroll">
+        <div className="mt-2 max-h-[300px] overflow-y-scroll">
+          <div className="flex bg-sky-50 text-xl text-sky-900 rounded-md shadow-md p-4 mb-4 sticky top-0">
+            <div className="w-1/5">
+              <h4>Patient Name</h4>
+            </div>
+            <div className="w-1/5">
+              <h4>Check-In ID</h4>
+            </div>
+            <div className="w-1/5">
+              <h4>Prescription Name</h4>
+            </div>
+            <div className="w-2/5">
+              <h4>Pharmacy Location</h4>
+            </div>
+            <div className="w-1/5">
+              <h4>Date Sent</h4>
+            </div>
+          </div>
+
+          {sentPrescriptions.map(renderPrescriptionRow)}
+        </div>
+      </div>
+
+      {/* <div className="mt-8 max-h-[300px] overflow-y-scroll">
         <div className="flex justify-between align-center">
           <h3 className="text-[24px]">Prescription dropped off</h3>
         </div>
@@ -91,7 +127,7 @@ export default function Prescription() {
 
           {sentPrescriptions.map(renderPrescriptionRow)}
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
