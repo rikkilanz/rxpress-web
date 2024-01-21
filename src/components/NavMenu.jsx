96a6725f13076168bd6ee343cbb4cc09aea160ec
Monica Zarate <monicazaratecortes@gmail.com>
2024-01-20 16:27:29 -0800

    Updated navigation logic.

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -15,22 +15,32 @@ const patientNav = [
     {name: "Pharmacies", path: "/pharmacies", icon: BuildingStorefrontIcon},               
 ];
 
+const pharmacyNav = [
+    {name: "Doctors", path: "/doctors", icon: IdentificationIcon},
+    {name: "Prescriptions", path: "/px", icon: DocumentTextIcon},
+    {name: "Pharmacies", path: "/pharmacies", icon: BuildingStorefrontIcon},               
+];
+
 export default function NavMenu({profile}) {
 
     const [selectedNav, setSelectedNav] = useState([]);
 
     useEffect(() => {
-        const selectedProfile = () => {
-            switch(profile){
-            case "doctor":
-                setSelectedNav(docNav);
-            break;
-            case "patient":
-                setSelectedNav(patientNav);
-            break;
-            }
-        };
-        selectedProfile();
+        // Rikki to update this once Landing Page logic is done
+        // const selectedProfile = () => {
+        //     switch(profile){
+        //     case "doctor":
+        //         setSelectedNav(docNav);
+        //     break;
+        //     case "patient":
+        //         setSelectedNav(patientNav);
+        //     break;
+        //     case "pharmacy":
+        //         setSelectedNav(pharmacyNav);
+        //     break;
+        //     }
+        // };
+        // selectedProfile();
 
     }, []);
 
@@ -39,11 +49,26 @@ export default function NavMenu({profile}) {
             <div className="mb-8">
                 <NavLink to="/" className="flex flex-col">
                     <img src={logo} alt="" className="h-16 w-auto mb-4" />
-                    <h1 className="font-bold uppercase text-sky-900">{profile}'s Dashboard</h1>
+                    
                 </NavLink>
             </div>
             <nav className="flex flex-col ">
-                {selectedNav.map((link) => (
+                <h1 className="font-bold uppercase text-sky-900 mb-4">Doctor's Nav</h1>
+                {docNav.map((link) => (
+                    <NavLink key={link.name} to={link.path} className="mb-8 text-xl text-sky-600 hover:text-sky-500 bg-sky-50/75 px-4 py-2 rounded-md outline-1 outline-sky-200/50 flex items-center outline">
+                        {<link.icon className="w-auto h-8 mr-4" />}
+                        <p className="">{link.name}</p>
+                    </NavLink>
+                ))}
+                <h1 className="font-bold uppercase text-sky-900 mb-4">Patient's Nav</h1>
+                {patientNav.map((link) => (
+                    <NavLink key={link.name} to={link.path} className="mb-8 text-xl text-sky-600 hover:text-sky-500 bg-sky-50/75 px-4 py-2 rounded-md outline-1 outline-sky-200/50 flex items-center outline">
+                        {<link.icon className="w-auto h-8 mr-4" />}
+                        <p className="">{link.name}</p>
+                    </NavLink>
+                ))}
+                <h1 className="font-bold uppercase text-sky-900 mb-4">Pharmacies Nav</h1>
+                {pharmacyNav.map((link) => (
                     <NavLink key={link.name} to={link.path} className="mb-8 text-xl text-sky-600 hover:text-sky-500 bg-sky-50/75 px-4 py-2 rounded-md outline-1 outline-sky-200/50 flex items-center outline">
                         {<link.icon className="w-auto h-8 mr-4" />}
                         <p className="">{link.name}</p>
